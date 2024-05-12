b2a29911f258ee7b0acdb5e52992abc7a4d25756
nad <31370689+nadflop@users.noreply.github.com>
2024-05-12 18:15:46 +0900

    Moved global score variables and playRound() function inside the newly created playGame() function

--- javascript.js
+++ javascript.js
@@ -1,7 +1,3 @@
-//keeping track of the players scores
-var humanScore = 0;
-var computerScore = 0;
-
 //getComputerChoice(): generates a random RPS choice for computer and returns it
 function getComputerChoice () {
     //Expected value for randomVal = 0, 1 or 2
@@ -37,49 +33,55 @@ function getHumanChoice () {
     return choice.toLowerCase();
 }
 
-//playRound(): takes human and computer players arguments, increments the round winner's score and logs a winner annoucement
-function playRound(humanChoice, computerChoice) {
-    //rock > scissors
-    //scissors > paper
-    //paper > rock
-    //same choice? tied. no points incremented
-    if (humanChoice == computerChoice) {
-        console.log(`It's a tie!`);
-    }
-    else {
-        switch (humanChoice) {
-            case `rock`:
-                if (computerChoice == `scissors`) {
-                    humanScore++;
-                    console.log("You win! Rock beats Scissors!");
-                }
-                else if (computerChoice == `paper`) { 
-                    computerScore++;
-                    console.log("You lose! Paper beats Rock.");
-                }
-                break;
-            case `paper`:
-                if (computerChoice == `rock`) {
-                    humanScore++;
-                    console.log("You win! Paper beats Rock!");
-                }
-                else if (computerChoice == `scissors`) { 
-                    computerScore++
-                    console.log("You lose! Scissors beats Paper.")
-                }
-                break;
-            case `scissors`:
-                if (computerChoice == `paper`) {
-                    humanScore++;
-                    console.log("You win! Scissors beats Paper!");
-                }
-                else if (computerChoice == `rock`) {
-                    computerScore++;
-                    console.log("You lose! Rock beats Scissors.")
-                }
-                break;
+function playGame() {
+    //keeping track of the players scores
+    var humanScore = 0;
+    var computerScore = 0;
+    //playRound(): takes human and computer players arguments, increments the round winner's score and logs a winner annoucement
+    function playRound(humanChoice, computerChoice) {
+        //rock > scissors
+        //scissors > paper
+        //paper > rock
+        //same choice? tied. no points incremented
+        if (humanChoice == computerChoice) {
+            console.log(`It's a tie!`);
+        }
+        else {
+            switch (humanChoice) {
+                case `rock`:
+                    if (computerChoice == `scissors`) {
+                        humanScore++;
+                        console.log("You win! Rock beats Scissors!");
+                    }
+                    else if (computerChoice == `paper`) { 
+                        computerScore++;
+                        console.log("You lose! Paper beats Rock.");
+                    }
+                    break;
+                case `paper`:
+                    if (computerChoice == `rock`) {
+                        humanScore++;
+                        console.log("You win! Paper beats Rock!");
+                    }
+                    else if (computerChoice == `scissors`) { 
+                        computerScore++
+                        console.log("You lose! Scissors beats Paper.")
+                    }
+                    break;
+                case `scissors`:
+                    if (computerChoice == `paper`) {
+                        humanScore++;
+                        console.log("You win! Scissors beats Paper!");
+                    }
+                    else if (computerChoice == `rock`) {
+                        computerScore++;
+                        console.log("You lose! Rock beats Scissors.")
+                    }
+                    break;
+            }
         }
     }
+
 }
 
 const humanSelection = getHumanChoice();
